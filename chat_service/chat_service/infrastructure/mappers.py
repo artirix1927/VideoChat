79d66e8e4397c9a14feb72395bd77089923a7667
Artyom Kotovskiy <mrartem1927@gmail.com>
2025-07-30 01:20:41 -0400

    Chat from chat model func

--- chat_service/chat_service/infrastructure/mappers.py
+++ chat_service/chat_service/infrastructure/mappers.py
@@ -1,6 +1,6 @@
 import stat
-from chat_service.domain.models import FriendRequest
-from chat_service.infrastructure.models import FriendRequestModel
+from chat_service.domain.models import Chat, FriendRequest
+from chat_service.infrastructure.models import ChatModel, FriendRequestModel
 
 
 def friend_request_from_model(friend_req: FriendRequestModel) -> FriendRequest:
@@ -9,4 +9,14 @@ def friend_request_from_model(friend_req: FriendRequestModel) -> FriendRequest:
         from_user=friend_req.from_user,
         to_user=friend_req.to_user,
         status=friend_req.status,
+    )
+
+
+def chat_from_model(chat: ChatModel) -> Chat:
+
+    return Chat(
+        id=chat.id,
+        is_group=chat.is_group,
+        members=chat.members,
+        created_at=chat.created_at,
     )
