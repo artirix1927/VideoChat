3558b755219da58fd90b9c7faeaa9a82940f5d8c
Artyom Kotovskiy <mrartem1927@gmail.com>
2025-10-02 15:37:18 -0400

    No navbar needed on call page

--- frontend/app/video/call/[id]/page.tsx
+++ frontend/app/video/call/[id]/page.tsx
@@ -1,9 +1,7 @@
-import { Navbar } from "@/components/Navbar";
 import VideoChat from "@/components/VideoChat";
 
 export default function VideoCall() {
   return <>
-    <Navbar></Navbar>
     <VideoChat></VideoChat>
     </>
     
